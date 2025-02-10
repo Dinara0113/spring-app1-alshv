65e6170da60608d938751fabcc3033685b0bfd4a
Dinara0113 <dinarkatttb16@gmail.com>
2025-02-10 19:20:49 +0500

    commit

--- spring-app1-alshv/src/main/java/com/alshv/springcourse/spring_app1_alshv/MusicPlayer.java
+++ spring-app1-alshv/src/main/java/com/alshv/springcourse/spring_app1_alshv/MusicPlayer.java
@@ -1,21 +1,23 @@
 package com.alshv.springcourse.spring_app1_alshv;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-    private RockMusic rockMusic;
-    private ClassicalMusic classicalMusic;
+    private Music music1;
+    private Music music2;
 
     @Autowired
-    public MusicPlayer(RockMusic rockMusic, ClassicalMusic classicalMusic) {
-        this.rockMusic = rockMusic;
-        this.classicalMusic = classicalMusic;
+    public MusicPlayer(@Qualifier("rockMusic") Music music1,
+                       @Qualifier("classicalMusic") Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
     }
 
     public String playMusic() {
-        return "Playing: " + classicalMusic.getSong();
+        return "Playing: " + music1.getSong() + ", " + music2.getSong();
 
     }
 }
